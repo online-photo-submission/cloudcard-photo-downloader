18ad46efa7dfc842235b3bd56fc7dcbb18c5cb69
B-Akumah <bakumah@liberty.edu>
2018-02-14 12:15:02 -0500

    our initial commit

---
+++ src/main/java/com/cloudcard/photoDownloader/PhotoDownloaderApplication.java
@@ -0,0 +1,12 @@
+package com.cloudcard.photoDownloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoDownloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoDownloaderApplication.class, args);
+	}
+}
