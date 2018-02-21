9c8172c339efd64942ed3f996fef3da13e650e08
B-Akumah <bakumah@liberty.edu>
2018-02-21 13:57:37 -0500

    Adding of Unirest

--- src/main/java/com/cloudcard/photoDownloader/PhotoDownloaderApplication.java
+++ src/main/java/com/cloudcard/photoDownloader/PhotoDownloaderApplication.java
@@ -1,7 +1,24 @@
 package com.cloudcard.photoDownloader;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.awt.*;
+
+/* List fetchPhotosReadyForDownload() {
+        RestResponse response = restBuilder.get("$apiURL/api/photos?status=$READY_FOR_DOWNLOAD") {
+            header "X-Auth-Token", accessToken
+        }
+
+        if (response.status != 200) {
+            throw new Exception("Error polling for downloadable images: $response.text")
+        }
+
+        return response.json
+    }*/
 
 @SpringBootApplication
 public class PhotoDownloaderApplication {
@@ -11,6 +28,13 @@ public class PhotoDownloaderApplication {
 		SpringApplication.run(PhotoDownloaderApplication.class, args);
 
 		System.out.println("Hello World");
+		//Unirest.shutdown();
+	}
+
+	List fetchPhotosReadyForDownload() {
+		//HttpResponse<JsonNode> jsonResponse = Unirest.post(
+
+		return null;
 	}
 
 }
