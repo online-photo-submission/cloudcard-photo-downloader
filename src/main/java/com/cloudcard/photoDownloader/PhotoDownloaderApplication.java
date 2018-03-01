bd2cd1cd007f94cec1530dec6b26c921e6ddc6e1
B-Akumah <bakumah@liberty.edu>
2018-03-01 12:44:27 -0500

    Added Unirest and functionality to hit cloudcard -still needs to receive access token

--- src/main/java/com/cloudcard/photoDownloader/PhotoDownloaderApplication.java
+++ src/main/java/com/cloudcard/photoDownloader/PhotoDownloaderApplication.java
@@ -1,40 +1,60 @@
 package com.cloudcard.photoDownloader;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.awt.*;
-
-/* List fetchPhotosReadyForDownload() {
-        RestResponse response = restBuilder.get("$apiURL/api/photos?status=$READY_FOR_DOWNLOAD") {
-            header "X-Auth-Token", accessToken
-        }
-
-        if (response.status != 200) {
-            throw new Exception("Error polling for downloadable images: $response.text")
-        }
-
-        return response.json
-    }*/
+import java.util.Scanner;
 
 @SpringBootApplication
 public class PhotoDownloaderApplication {
 
 
-	public static void main(String[] args) {
+    public static final String API_URL = "https://app.onlinephotosubmission.com/api";
+
+    public static void main(String[] args) {
 		SpringApplication.run(PhotoDownloaderApplication.class, args);
 
-		System.out.println("Hello World");
+        HttpResponse<JsonNode> myList;
+        myList = fetchPhotosReadyForDownload();
+
+        System.out.println(myList);
+
+        System.out.println("Hello World");
 		//Unirest.shutdown();
 	}
 
-	List fetchPhotosReadyForDownload() {
-		//HttpResponse<JsonNode> jsonResponse = Unirest.post(
 
-		return null;
-	}
+
+    private static HttpResponse<JsonNode> fetchPhotosReadyForDownload() throws UnirestException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Username: ");
+        String Username = sc.nextLine();
+
+        System.out.println("Enter Password: ");
+        String Password = sc.nextLine();
+
+
+        HttpResponse<JsonNode> adminDesc = Unirest.post(API_URL + "/login")
+                .body("{username:" + Username + ", password: " + Password + "}").asJson();
+               /* .body({"username":Username, "password":Password}).asJson(); */
+
+        String accessToken = adminDesc.getBody().toString();
+
+        try {
+            HttpResponse<JsonNode> jsonImageResponse = Unirest.post(API_URL +"/photos?status=READY_FOR_DOWNLOAD")
+            .header("accept", "application/json").header("X-Auth-Token", accessToken).asJson();
+
+            return jsonImageResponse;
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+
+
+        return null;
+    }
 
 }
