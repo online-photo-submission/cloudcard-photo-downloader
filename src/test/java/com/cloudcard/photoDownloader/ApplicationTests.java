597d8cd1fc462939efb623548a5870500416df54
Tony Erskine <tony@sharptop.co>
2019-01-25 13:01:59 -0500

    removed ignore from context loads test

--- src/test/java/com/cloudcard/photoDownloader/ApplicationTests.java
+++ src/test/java/com/cloudcard/photoDownloader/ApplicationTests.java
@@ -1,12 +1,10 @@
 package com.cloudcard.photoDownloader;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@Ignore
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class ApplicationTests {
