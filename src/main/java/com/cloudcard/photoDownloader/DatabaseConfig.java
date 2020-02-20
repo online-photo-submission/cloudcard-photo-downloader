ed9d6bfbf058918c9a45dd081d61538442e9acf8
Tony Erskine <tony@sharptop.co>
2020-02-20 09:26:30 -0500

    made the DB config conditional

--- src/main/java/com/cloudcard/photoDownloader/DatabaseConfig.java
+++ src/main/java/com/cloudcard/photoDownloader/DatabaseConfig.java
@@ -1,6 +1,7 @@
 package com.cloudcard.photoDownloader;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -9,6 +10,7 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import javax.sql.DataSource;
 
 @Configuration
+@ConditionalOnProperty(value = "db.datasource.enabled", havingValue = "true")
 public class DatabaseConfig {
 
     @Value("${db.datasource.driverClassName}")
