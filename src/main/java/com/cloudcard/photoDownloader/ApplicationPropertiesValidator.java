31b8cbcb5970d2f2d9e7eccb326e20dc35204054
Tony Erskine <tony@sharptop.co>
2020-02-20 09:27:27 -0500

    20.02.20

--- src/main/java/com/cloudcard/photoDownloader/ApplicationPropertiesValidator.java
+++ src/main/java/com/cloudcard/photoDownloader/ApplicationPropertiesValidator.java
@@ -12,7 +12,7 @@ public class ApplicationPropertiesValidator {
 
     private static final Logger log = LoggerFactory.getLogger(ApplicationPropertiesValidator.class);
 
-    String version = "20.02.17.1557";
+    String version = "20.02.20";
 
     @Value("${downloader.delay.milliseconds}")
     private Integer downloaderDelay;
