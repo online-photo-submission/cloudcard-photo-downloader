05d5df1fff3502331280accb67f2578747e9184f
Tony Erskine <tony@sharptop.co>
2020-02-28 16:54:06 -0500

    Deprecated Links

--- src/main/java/com/cloudcard/photoDownloader/Links.java
+++ src/main/java/com/cloudcard/photoDownloader/Links.java
@@ -1,17 +1,14 @@
 package com.cloudcard.photoDownloader;
 
+import com.fasterxml.jackson.annotation.*;
+
 import java.util.HashMap;
 import java.util.Map;
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+@Deprecated
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
-        "bytes"
+    "bytes"
 })
 public class Links {
 
@@ -22,21 +19,25 @@ public class Links {
 
     @JsonProperty("bytes")
     public String getBytes() {
+
         return bytes;
     }
 
     @JsonProperty("bytes")
     public void setBytes(String bytes) {
+
         this.bytes = bytes;
     }
 
     @JsonAnyGetter
     public Map<String, Object> getAdditionalProperties() {
+
         return this.additionalProperties;
     }
 
     @JsonAnySetter
     public void setAdditionalProperty(String name, Object value) {
+
         this.additionalProperties.put(name, value);
     }
 
