6f09b1be15ea5fb7b5beeb31510c582a4e30fbf2
Tony Erskine <tony@sharptop.co>
2020-03-09 10:29:01 -0400

    improved clean.sh, so I can trigger it automatically during development

--- clean.sh
+++ clean.sh
@@ -1,4 +1,14 @@
 rm *.jpg
 rm C*.udf
 rm *.txt
+echo .
+echo Temp Directory
+ls temp
 rm -rf temp
+echo .
+echo Summary Directory
+ls summary
+echo .
+echo Summary File Contents
+cat summary/*.txt
+rm -rf summary
