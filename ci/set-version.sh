8a48007b5bd9c901bd506c3a2efe0eee7e1e720d
Tony Erskine <tony@sharptop.co>
2020-03-09 11:18:42 -0400

    working on release script

--- ci/set-version.sh
+++ ci/set-version.sh
@@ -5,15 +5,17 @@ set -e
 version="$1"
 
 if [ -z "$1" ]; then
-    version="$(date +"%y.%m.%d.%H%M")"
+  version="$(date +"%y.%m.%d.%H%M")"
 fi
 
 echo "Setting version to $version"
 
-#if [[ -n $(git status -s) ]]; then
-#    echo "Please make sure git is in a clean state before running this script."
-#    exit
-#fi
+if [[ -n $(git status -s) ]]; then
+  echo "Please make sure git is in a clean state before running this script."
+  exit
+fi
+
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/amazon-corretto-8.jdk/Contents/Home
 
 GRADLE_CONF="build.gradle"
 PROP_CONF="src/main/java/com/cloudcard/photoDownloader/ApplicationPropertiesValidator.java"
@@ -25,8 +27,16 @@ sed -i"$BACKUP" -e "s/version = .*$/version = \'$version\'/" $GRADLE_CONF
 rm "${PROP_CONF}$BACKUP"
 rm "${GRADLE_CONF}$BACKUP"
 
+# build
+rm cloudcard-photo-downloader.jar
+./gradlew clean test build
+./clean.sh
+cp build/libs/cloudcard-photo-downloader-*.jar cloudcard-photo-downloader.jar
+
+# commit
+git add cloudcard-photo-downloader.jar
 git add $PROP_CONF
 git add $GRADLE_CONF
 
-git commit -m "$version"
-git tag "$version"
+#git commit -m "$version"
+#git tag "$version"
