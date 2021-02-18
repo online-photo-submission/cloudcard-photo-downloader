a7648e0d61aae5a7d218396d21eb2dd8b58e2b41
Tony Erskine <tony@sharptop.co>
2021-02-18 05:59:38 -0500

    added example run.bat

---
+++ run.bat
@@ -0,0 +1,5 @@
+java ^
+ -Dcloudcard.api.accessToken="AccessTokenAccessTokenAccessToken" ^
+ -Ddownloader.fetchStatuses="READY_FOR_DOWNLOAD" ^
+ -Ddownloader.photoDirectories="Z:\path\to\your\picture\storage" ^
+ -jar cloudcard-photo-downloader.jar
