925f4feafa3190e8c28801239fde64b7d157c821
Tony Erskine <tony@sharptop.co>
2021-06-11 11:11:34 -0400

    fixed bug where exceptions caused downloader.repeat to be ignored, which could create rogue processes

--- src/main/java/com/cloudcard/photoDownloader/DownloaderService.java
+++ src/main/java/com/cloudcard/photoDownloader/DownloaderService.java
@@ -56,24 +56,36 @@ public class DownloaderService {
     @Scheduled(fixedDelayString = "${downloader.delay.milliseconds}")
     public void downloadPhotos() throws Exception {
 
-        log.info("  ==========  Downloading photos  ==========  ");
-        shellCommandService.preExecute();
-        List<Photo> photosToDownload = cloudCardPhotoService.fetchReadyForDownload();
-        shellCommandService.preDownload(photosToDownload);
-        List<PhotoFile> downloadedPhotoFiles = storageService.save(photosToDownload);
-        for (PhotoFile photoFile : downloadedPhotoFiles) {
-            Photo downloadedPhoto = new Photo(photoFile.getPhotoId());
-            cloudCardPhotoService.markAsDownloaded(downloadedPhoto);
-        }
+        int exitStatus = 0;
+        try {
 
-        summaryService.createSummary(photosToDownload, downloadedPhotoFiles);
-        shellCommandService.postDownload(downloadedPhotoFiles);
-        shellCommandService.postExecute();
-        log.info("Completed downloading " + downloadedPhotoFiles.size() + " photos.");
+            log.info("  ==========  Downloading photos  ==========  ");
+            shellCommandService.preExecute();
+            List<Photo> photosToDownload = cloudCardPhotoService.fetchReadyForDownload();
+            shellCommandService.preDownload(photosToDownload);
+            List<PhotoFile> downloadedPhotoFiles = storageService.save(photosToDownload);
+            for (PhotoFile photoFile : downloadedPhotoFiles) {
+                Photo downloadedPhoto = new Photo(photoFile.getPhotoId());
+                cloudCardPhotoService.markAsDownloaded(downloadedPhoto);
+            }
 
-        if (!repeat) {
-            log.info("downloader.repeat is set to false. Exiting application now.");
-            System.exit(0);
+            summaryService.createSummary(photosToDownload, downloadedPhotoFiles);
+            shellCommandService.postDownload(downloadedPhotoFiles);
+            shellCommandService.postExecute();
+            log.info("Completed downloading " + downloadedPhotoFiles.size() + " photos.");
+
+        } catch (Exception e) {
+
+            log.error(e.getMessage());
+            e.printStackTrace();
+            exitStatus = 1;
+
+        } finally {
+
+            if (!repeat) {
+                log.info("downloader.repeat is set to false. Exiting application now.");
+                System.exit(exitStatus);
+            }
         }
     }
 }
