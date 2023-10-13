defd647c5eb9b434db5864d7b847fb0a10d5daf8
Ryan Heathcote <ryan@cloudcard.us>
2023-10-13 14:17:37 -0400

    23.10.13

--- src/main/groovy/com/cloudcard/photoDownloader/ApplicationPropertiesValidator.java
+++ src/main/groovy/com/cloudcard/photoDownloader/ApplicationPropertiesValidator.java
@@ -7,7 +7,7 @@ public class ApplicationPropertiesValidator {
 
     private static final Logger log = LoggerFactory.getLogger(ApplicationPropertiesValidator.class);
 
-    public static String version = "23.10.12";
+    public static String version = "23.10.13";
 
     public static void logVersion() {
 
