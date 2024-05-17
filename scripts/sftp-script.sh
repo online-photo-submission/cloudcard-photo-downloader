21a53cabf90e54f1ba69296d71073a91605f0eb7
Jonathan Hoffman <jonathan@cloudcard.us>
2024-05-17 14:22:02 -0400

    Updated sftp script to only send jpg and csv files

--- scripts/sftp-script.sh
+++ scripts/sftp-script.sh
@@ -8,7 +8,8 @@ PHOTO_DIRECTORY=downloaded-photos
 set -e
 
 if test ! -f $FILE; then
-    echo "put """$PHOTO_DIRECTORY"""/*" > "$FILE"
+    echo "put """$PHOTO_DIRECTORY"""/*.jpg" > "$FILE"
+    echo "put """$PHOTO_DIRECTORY"""/*.csv" >> "$FILE"
 fi
 
 sftp -i "$KEY" -b "$FILE" sftp://$USER@$HOST/$DESTINATION_DIRECTORY
