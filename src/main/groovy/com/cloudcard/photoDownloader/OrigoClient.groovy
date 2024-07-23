97a2c5013f82dbb3404479bb1e13178e36532c70
ben-w-martin <ben.w.martin17@gmail.com>
2024-07-23 18:40:09 -0400

    committing to access on different device

--- src/main/groovy/com/cloudcard/photoDownloader/OrigoClient.groovy
+++ src/main/groovy/com/cloudcard/photoDownloader/OrigoClient.groovy
@@ -55,60 +55,55 @@ class OrigoClient extends OrigoService {
 
     String callbackUrl
 
-    Map requestHeaders
-
     @PostConstruct
-    def init() {
-            throwIfBlank(eventManagementApi, "The Origo Event Management API URL must be specified.")
-            throwIfBlank(callbackRegistrationApi, "The Origo Callback Registration API URL must be specified.")
-            throwIfBlank(mobileIdentitiesApi, "The Origo Mobile Identities API URL must be specified.")
-            throwIfBlank(customerId, "The Origo customer ID must be specified.")
-            throwIfBlank(organizationId, "The Origo organization ID must be specified.")
-            throwIfBlank(accessToken, "An Origo access token must be provided.")
-            throwIfBlank(contentType, "The Origo content-type header must be specified.")
-            throwIfBlank(applicationVersion, "The Origo application version must be specified.")
-            throwIfBlank(applicationId, "Your organization's Origo Application ID must be specified.")
+    init() {
+        throwIfBlank(eventManagementApi, "The Origo Event Management API URL must be specified.")
+        throwIfBlank(callbackRegistrationApi, "The Origo Callback Registration API URL must be specified.")
+        throwIfBlank(mobileIdentitiesApi, "The Origo Mobile Identities API URL must be specified.")
+        throwIfBlank(customerId, "The Origo customer ID must be specified.")
+        throwIfBlank(organizationId, "The Origo organization ID must be specified.")
+        throwIfBlank(authorization, "The Origo authorization string must be specified.")
+        throwIfBlank(accessToken, "An Origo access token must be provided.")
+        throwIfBlank(contentType, "The Origo content-type header must be specified.")
+        throwIfBlank(applicationVersion, "The Origo application version must be specified.")
+        throwIfBlank(applicationId, "Your organization's Origo Application ID must be specified.")
 
-            log.info('=================== Accessing HID Origo Services ===================')
-            log.info("          Origo Event Management API URL : $eventManagementApi")
-            log.info("     Origo Callback Registration API URL : $callbackRegistrationApi")
-            log.info("         Origo Mobile Identities API URL : $mobileIdentitiesApi")
-            log.info("                       Origo customer ID : $customerId")
-            log.info("                   Origo organization ID : $organizationId")
-            log.info("              Origo authorization string : $authorization")
-            log.info("               Origo content type header : $contentType")
-            log.info("               Origo application version : $applicationVersion")
-            log.info("                    Origo application ID : $applicationId")
-
-            requestHeaders = [
-                    'Authorization' : authorization,
-                    'Content-Type' : contentType,
-                    'Application-Version' : applicationVersion,
-                    'Application-ID' : applicationId
-            ]
-
+        log.info('=================== Accessing HID Origo Services ===================')
+        log.info("          Origo Event Management API URL : $eventManagementApi")
+        log.info("     Origo Callback Registration API URL : $callbackRegistrationApi")
+        log.info("         Origo Mobile Identities API URL : $mobileIdentitiesApi")
+        log.info("                       Origo customer ID : $customerId")
+        log.info("                   Origo organization ID : $organizationId")
+//        log.info("              Origo authorization string : $authorization")
+        log.info("               Origo content type header : $contentType")
+        log.info("               Origo application version : $applicationVersion")
+        log.info("                    Origo application ID : $applicationId")
     }
 
-    def createCallbackSubscription() {
+    createCallbackSubscription() {
         // Initial action of OrigoIntegration application - Requests all users for given organization.
 
-        HttpResponse<String> response
+        Map requestHeaders = [
+                'Authorization'      : authorization,
+                'Content-Type'       : contentType,
+                'Application-Version': applicationVersion,
+                'Application-ID'     : applicationId
+        ]
+
         String serializedBody = new ObjectMapper().writeValueAsString([
-            "url": "$callbackUrl",
-            "filterId": "$filterId",
-            "httpHeader": "Authorization",
-            "secret": "$authorization"
+                url       : "$callbackUrl", // SQS ???
+                filterId  : "$filterId",
+                httpHeader: "Authorization",
+                secret    : "$authorization"
         ])
+
+        HttpResponse<String> response
 
         try {
             response = Unirest.post(eventManagementApi + "/organization/$organizationId/callback")
                     .headers(requestHeaders)
                     .body(serializedBody)
                     .asString()
-
-//            if (response.status != 201) {
-//                log.error() pending shape of response
-//            }
 
             log.info("Response: $response")
 
@@ -117,27 +112,47 @@ class OrigoClient extends OrigoService {
         }
     }
 
-    static def uploadUserPhoto() {
+    uploadUserPhoto() {
         // posts photo to User's Origo profile: https://doc.origo.hidglobal.com/api/mobile-identities/#/Photo%20ID/post-customer-organization_id-users-user_id-photo
     }
 
-    static def checkExistingCallbackFns() {
+    checkExistingCallbackSubs() {
         // checks for existing call back subscriptions.
+
+        Map requestHeaders = [
+                'Authorization'      : authorization,
+                'Content-Type'       : contentType,
+                'Application-Version': applicationVersion,
+                'Application-ID'     : applicationId
+        ]
+
+        HttpResponse<String> response
+
+        try {
+            response = Unirest.get(eventManagementApi + "/organization/$organizationId/callback")
+                    .headers(requestHeaders)
+                    .asString()
+
+            log.info("Response: $response")
+
+        } catch (HttpException e) { // ?
+            log.error(e.message)
+        }
     }
 
-    static def checkExistingFilters() {
+    checkExistingFilters() {
         // checks for current filters. Conditionally calls create filter
     }
 
-    static def createFilter() {
+    createFilter() {
         // Filters what kinds of events this instance of the application will be subscribed to. See documentation for options: https://doc.origo.hidglobal.com/api/events-callbacks/#/Events/post_organization__organization_id__events_filter
     }
 
-    static def storePhoto() {
+    storePhoto() {
         // Stores photo in Origo system
     }
 
-    static def storePersonData() {
+    storePersonData() {
         // stores 'customFields' information in Origo employee record
     }
 
