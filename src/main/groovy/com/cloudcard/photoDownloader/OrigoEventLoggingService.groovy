f724f9af1bbe8018cb3be40d87d0660010cadb71
ben-w-martin <ben.w.martin17@gmail.com>
2024-07-26 16:12:57 -0400

    add: OrigoEventLoggingService creates max of last 2 request logs, deletes 3rd oldest.

--- src/main/groovy/com/cloudcard/photoDownloader/OrigoEventLoggingService.groovy
+++ src/main/groovy/com/cloudcard/photoDownloader/OrigoEventLoggingService.groovy
@@ -1,13 +1,11 @@
 package com.cloudcard.photoDownloader
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import groovy.json.JsonOutput
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Component
 
-import javax.swing.text.DateFormatter
 import java.text.SimpleDateFormat
 
 
@@ -20,60 +18,121 @@ class OrigoEventLoggingService {
 
     static File _eventLogDirectory
 
-    static File _eventLog
+    static List<File> _eventLogs
 
-    static configureLocalLogging() {
+    static removeOldLogs() {
+        // deletes 3rd oldest log file. keeps locally stored log files always at 2 or less.
+        // currently only works with .json files OR 4-letter file extensions
+        File[] files = _eventLogDirectory.listFiles()
+        def fileNames
+
+        if (files.size() >= 3) {
+            fileNames = new ArrayList<long>()
+
+            files.each {
+                long parsed = it.name.substring(0, it.name.length() - 5) as long
+                fileNames.add(parsed)
+            }
+
+            fileNames.sort {
+                a, b -> b <=> a
+            }
+
+            fileNames = fileNames[0..1]
+
+            ArrayList<File> filesToKeep = new ArrayList<File>()
+
+            files.each {
+                file ->
+                    {
+                        if (file.name != fileNames[0] + ".json" && file.name != fileNames[1] + ".json") {
+                            log.info("**************************************")
+                            log.info("DELETING OLD ORIGO EVENT LOGS: ${file.name}")
+                            log.info("**************************************")
+                            file.delete()
+                        } else {
+                            if (_eventLogs?.size() == 0 || _eventLogs == null) {
+                                _eventLogs = new ArrayList<File>()
+                            }
+
+                            filesToKeep.add(file)
+                        }
+                    }
+            }
+
+            _eventLogs = filesToKeep
+            _eventLogs.each {log.info("EVENT LOGS ARRAY: " + it.name)}
+
+        }
+
+    }
+
+    static createLogDirectory() {
         // creates directory and log file if they don't exist
 
-        File eventLogDirectory = new File(System.getProperty("user.dir").toString() + "/origo-event-log")
+        File eventLogDirectory = new File("${System.getProperty("user.dir").toString()}/origo-event-logs")
 
         if (eventLogDirectory.exists() && eventLogDirectory.isDirectory()) {
-            log.info('origo-event-log DIRECTORY EXISTS')
+            log.info('origo-event-logs DIRECTORY EXISTS.')
         } else {
-            log.info('origo-event-log DIRECTORY DOESN\'T EXIST: Creating directory with log.json')
+            log.info('origo-event-logs DIRECTORY DOESN\'T EXIST: Creating directory.')
             eventLogDirectory.mkdir()
         }
 
         _eventLogDirectory = eventLogDirectory
     }
 
-    static boolean createJsonLog(String path, String fileName) {
+    static File createLogFile(String path, String fileName) {
         File eventLog = new File("${path}/${fileName}.json")
 
-        boolean result = eventLog.createNewFile()
+        eventLog.createNewFile()
 
-        _eventLog = eventLog
-
-        return result
+        return eventLog
     }
 
     def getLastEvent() {}
 
-    static writeEventsToJson(List<Map> events) {
+    static generateLog(List<Map> events) {
+        // takes in list of event date:id objects, creates json file and writes log.
+        // json file takes of the name of the oldest event in the log in milliseconds
 
         def sortedEvents = events.sort {
             it.date
         }
 
-        def isoDateFormat = new SimpleDateFormat('yyyy-MM-dd\'T\'HH:mm:ss')
-        isoDateFormat.setTimeZone(TimeZone.getTimeZone('UTC'))
+        def oldestEventDate = sortedEvents[0].date.toString()
 
-        def fileName = isoDateFormat.parse(sortedEvents[0].date.toString()).time
-
-        log.info("$fileName")
+        String fileName = isoToMilliseconds(oldestEventDate)
 
         String json = JsonOutput.toJson(sortedEvents)
 
-        boolean created = createJsonLog("${_eventLogDirectory.getPath().toString()}", "${fileName}")
+        File eventLog = createLogFile("${_eventLogDirectory.getPath().toString()}", "${fileName}")
 
-        if (created) {
-            _eventLog.withWriter {
+        if (eventLog.exists()) {
+            eventLog.withWriter {
                 writer -> writer.writeLine json
             }
         } else {
             log.error("Origo Error while creating log file")
         }
 
+    }
+
+    private static String isoToMilliseconds(String date) {
+        def isoDateFormat = new SimpleDateFormat('yyyy-MM-dd\'T\'HH:mm:ss')
+        isoDateFormat.setTimeZone(TimeZone.getTimeZone('UTC'))
+
+        def fileName = isoDateFormat.parse(date).time.toString()
+        return fileName
+    }
+
+    private static String millisecondsToIso(long milliseconds) {
+        def isoDateFormat = new SimpleDateFormat('yyyy-MM-dd\'T\'HH:mm:ss')
+        isoDateFormat.setTimeZone(TimeZone.getTimeZone('UTC'))
+
+        def date = new Date(milliseconds)
+        def isoDate = isoDateFormat.format(date)
+        return isoDate
     }
 
     def getEventById() {}
