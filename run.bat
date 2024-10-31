ed586b50b3fc79507234c8bdea53d9d5e24f1fa3
ChristianDuncan1 <christian@cloudcard.us>
2024-10-31 11:09:48 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,1 @@
-java ^
- -Dcloudcard.api.accessToken="AccessTokenAccessTokenAccessToken" ^
- -Ddownloader.fetchStatuses="READY_FOR_DOWNLOAD" ^
- -Ddownloader.photoDirectories="Z:\path\to\your\picture\storage" ^
- -jar cloudcard-photo-downloader.jar
+java -jar cloudcard-photo-downloader.jar
