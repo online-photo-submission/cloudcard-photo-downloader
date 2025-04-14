a21cbd64edb63145b616990c903b1a0d21b9c65e
Jonathan Hoffman <jonathan@cloudcard.us>
2025-04-14 16:06:44 -0400

    25.04.14.1606

--- src/main/groovy/com/cloudcard/photoDownloader/ApplicationPropertiesValidator.java
+++ src/main/groovy/com/cloudcard/photoDownloader/ApplicationPropertiesValidator.java
@@ -7,7 +7,7 @@ public class ApplicationPropertiesValidator {
 
     private static final Logger log = LoggerFactory.getLogger(ApplicationPropertiesValidator.class);
 
-    public static String version = "24.12.11.1425";
+    public static String version = "25.04.14.1606";
 
     public static void logVersion() {
 
