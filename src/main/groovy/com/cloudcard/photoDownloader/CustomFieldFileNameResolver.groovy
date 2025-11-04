f9ffba42196c20ca5fb868b6922a9618e0c2faef
Jonathan Hoffman <jonathan@cloudcard.us>
2025-11-04 16:14:30 -0500

    Adds ability to get a photo's date created from the custom field file name resolver

---
+++ src/main/groovy/com/cloudcard/photoDownloader/CustomFieldFileNameResolver.groovy
@@ -0,0 +1,64 @@
+package com.cloudcard.photoDownloader
+
+import jakarta.annotation.PostConstruct
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.stereotype.Service
+
+import java.text.SimpleDateFormat
+
+import static com.cloudcard.photoDownloader.ApplicationPropertiesValidator.throwIfTrue
+
+@Service
+@ConditionalOnProperty(value = "downloader.fileNameResolver", havingValue = "CustomFieldFileNameResolver")
+class CustomFieldFileNameResolver implements FileNameResolver {
+
+    private static final Logger log = LoggerFactory.getLogger(CustomFieldFileNameResolver.class)
+
+    @Value('${downloader.minPhotoIdLength}')
+    private Integer minPhotoIdLength
+
+    @Value('${CustomFieldFileNameResolver.include}')
+    String[] include
+
+    @Value('${CustomFieldFileNameResolver.delimiter}')
+    String delimiter
+
+    @Value('${CustomFieldFileNameResolver.fileNameDateFormat:YYYY-MM-DD_HH-MM-SS}')
+    String fileNameDateFormat
+
+    @PostConstruct
+    void init() {
+        throwIfTrue(include == null || include.length == 0, "The Custom Field(s) must be specified.")
+        log.info("Include Custom Fields : " + include.length + " " + (Arrays.toString(include)))
+        log.info("Custom Field Delimiter: '" + delimiter + "'")
+    }
+
+    @Override
+    String getBaseName(Photo photo) {
+        Map<String, String> customFields = photo.getPerson().getCustomFields()
+        String fileName = ""
+
+        for (String i : include) {
+            if (!fileName.equals("")) {
+                fileName = fileName + delimiter
+            }
+            if (i.equals("dateCreated")) {
+                fileName = fileName + new SimpleDateFormat(fileNameDateFormat).format(photo.dateCreated)
+            } else if (i.equals("identifier")) {
+                fileName = fileName + photo.getPerson().getIdentifier()
+            } else {
+                String customFieldValue = customFields.get(i)
+                if (customFieldValue == null || customFieldValue.isEmpty()) {
+                    log.error("Person: " + photo.getPerson().getEmail() + " is missing a value for " + i)
+                    return null
+                }
+                fileName = fileName + customFieldValue
+            }
+        }
+        return fileName
+    }
+
+}
