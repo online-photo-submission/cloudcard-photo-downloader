30e3b8f0f5bb8327d8815e906419b45cef2ee184
Ryan Heathcote <ryan@cloudcard.us>
2026-02-25 10:17:56 -0500

    remove unused import.

--- src/main/groovy/com/cloudcard/photoDownloader/AdditionalPhotoPostProcessor.java
+++ src/main/groovy/com/cloudcard/photoDownloader/AdditionalPhotoPostProcessor.java
@@ -1,6 +1,5 @@
 package com.cloudcard.photoDownloader;
 
-import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
