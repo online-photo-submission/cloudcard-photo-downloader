4908b21e87678541c5956408dc34e52adac03d7d
Cameron Seals <cam@cloudcard.us>
2026-04-28 13:23:26 -0400

    - just a whole buncha things to make the thing work

--- src/main/groovy/com/cloudcard/photoDownloader/RemoteConfigInitializer.groovy
+++ src/main/groovy/com/cloudcard/photoDownloader/RemoteConfigInitializer.groovy
@@ -1,6 +1,8 @@
 package com.cloudcard.photoDownloader
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import kong.unirest.core.HttpResponse
 import kong.unirest.core.Unirest
@@ -13,31 +15,42 @@ import org.springframework.core.env.MapPropertySource
 
 class RemoteConfigInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
     private static final Logger log = LoggerFactory.getLogger(RemoteConfigInitializer.class)
+    ObjectMapper jacksonMapper = new ObjectMapper();
+    private String authToken;
 
     @Override
     void initialize(ConfigurableApplicationContext context) {
         ConfigurableEnvironment env = context.environment
+        jacksonMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
         // 1. Get seed info from local env (PAT and API URL)
         String pat = env.getProperty("cloudcard.api.accessToken")
         String apiUrl = env.getProperty("cloudcard.api.url")
+        String integrationName = env.getProperty("cloudcard.integration.name")
 
         // 2. Fetch the JSON from your API
-        Map<String, Object> remoteProperties = fetchRemoteConfig(apiUrl, pat)
+        Map<String, Object> remoteProperties = fetchRemoteConfig(apiUrl, pat, integrationName)
 
         // 3. Inject into the environment at the highest priority
         env.getPropertySources().addFirst(new MapPropertySource("remoteApiConfig", remoteProperties));
     }
 
-    private Map<String, Object> fetchRemoteConfig(String apiUrl, String pat) {
-        String authToken = login(apiUrl, pat);
+    private Map<String, Object> fetchRemoteConfig(String apiUrl, String pat, String integrationName) {
+        authToken = login(apiUrl, pat);
         log.info("Successfully retrieved auth token from CloudCard API. ${authToken}");
 //       TODO: Make the actual API call to fetch the config using authToken
 
-        return [
-            "downloader.fetchStatuses": "APPROVED",
-            "downloader.putStatus": "DOWNLOADED"
-        ]
+        String url =  apiUrl + "/integrations/$integrationName?findBy=name";
+        HttpResponse<String> response = Unirest.get(url).headers(standardHeaders(true)).asString();
+
+        if (response.getStatus() != 200) {
+            log.error("Status " + response.getStatus() + " returned from CloudCard API when retrieving integration.");
+            return;
+        }
+
+        Integration integration = new ObjectMapper().readValue(response.getBody(), new TypeReference<Integration>(){});
+
+        return integration.integrationConfigs.collectEntries { [it.propertyName, it.propertyValue] }
     }
 //        TODO: This duplicates code from the token service. Consider refactoring into a utility class or something.
     private String login(String apiUrl, String persistentAccessToken) throws Exception {
@@ -60,7 +73,20 @@ class RemoteConfigInitializer implements ApplicationContextInitializer<Configura
         Map<String, String> headers = new HashMap<>();
         headers.put("accept", "application/json");
         headers.put("Content-Type", "application/json");
-//        if (includeToken) headers.put("X-Auth-Token", getAuthToken());
+        if (includeToken) headers.put("X-Auth-Token", authToken);
         return headers;
     }
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+class Integration {
+    String name
+    String integrationType
+    List<IntegrationConfig> integrationConfigs
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+class IntegrationConfig {
+    String propertyName
+    String propertyValue
 }
