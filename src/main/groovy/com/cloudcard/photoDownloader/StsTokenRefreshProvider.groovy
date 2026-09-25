b6aea9b3f2e6dfc4dadd3ce476bb476edeb2d5b3
Jonathan Hoffman <jonathan@cloudcard.us>
2026-09-25 15:43:38 -0400

    Cherry picking some cleanups

--- src/main/groovy/com/cloudcard/photoDownloader/StsTokenRefreshProvider.groovy
+++ src/main/groovy/com/cloudcard/photoDownloader/StsTokenRefreshProvider.groovy
@@ -1,5 +1,6 @@
 package com.cloudcard.photoDownloader
 
+import groovy.transform.TupleConstructor
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import software.amazon.awssdk.auth.credentials.AwsCredentials
@@ -20,7 +21,7 @@ class StsTokenRefreshProvider implements AwsCredentialsProvider {
     private final String queueUrl
     private final Clock clock
 
-    private volatile StsSession currentSession
+    StsSession currentSession
 
     StsTokenRefreshProvider(CloudCardClient cloudCardClient, String queueUrl, Clock clock = Clock.systemUTC()) {
         this.cloudCardClient = cloudCardClient
@@ -78,15 +79,10 @@ class StsTokenRefreshProvider implements AwsCredentialsProvider {
         return false
     }
 
+    @TupleConstructor
     private static final class StsSession {
         final AwsSessionCredentials credentials
         final Instant refreshAt
         final Instant expiresAt
-
-        StsSession(AwsSessionCredentials credentials, Instant refreshAt, Instant expiresAt) {
-            this.credentials = credentials
-            this.refreshAt = refreshAt
-            this.expiresAt = expiresAt
-        }
     }
 }
